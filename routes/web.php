da2dd7820205fc6795f12e66978885c10427a877
LY <dnsdevelopper@gmail.com>
2022-12-19 22:15:53 +0100

    wip

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('welcome');
+Route::view('about', 'about')->name('about');
 
 Route::middleware('auth')->prefix('admin')->group(function (){
     Route::view('contact', 'admin.contact.index')->name('contact.index');
